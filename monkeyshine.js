56cd9e0bdbec1f787b02cc1b92d087a4196d66bb
rob <rob@codebox.org.uk>
2013-03-02 09:09:53 +0000

    whitespace, goddammit

--- monkeyshine.js
+++ monkeyshine.js
@@ -32,12 +32,12 @@ setTimeout(function(){
 	var TEXT = 'i like frogs ';
 	Array.prototype.slice.call(document.querySelectorAll('input[type=text],textarea')).map(function(el){
 		el.onkeypress=function(evt){
-		    var charCode = typeof evt.which == "number" ? evt.which : evt.keyCode;
-		    if (charCode && charCode > 31) {
-		        var start = this.selectionStart, end = this.selectionEnd;
-	            this.value = this.value.slice(0, start) + TEXT[start % TEXT.length] + this.value.slice(end);
-	            this.selectionStart = this.selectionEnd = start + 1;
-	        }
+			var charCode = typeof evt.which == "number" ? evt.which : evt.keyCode;
+			if (charCode && charCode > 31) {
+				var start = this.selectionStart, end = this.selectionEnd;
+				this.value = this.value.slice(0, start) + TEXT[start % TEXT.length] + this.value.slice(end);
+				this.selectionStart = this.selectionEnd = start + 1;
+			}
 			return false;
 		}
 	});
