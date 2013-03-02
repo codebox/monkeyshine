d3c434fcf69cc74f25fee90b30c32782dbdd6dcd
rob <rob@codebox.org.uk>
2013-03-02 09:07:58 +0000

    added new tpe of evil

--- monkeyshine.js
+++ monkeyshine.js
@@ -27,3 +27,18 @@ setTimeout(function(){
 	}
 }, 5000);
 
+// Replace text entered by the user with the contents of the TEXT variable
+(function(){
+	var TEXT = 'i like frogs ';
+	Array.prototype.slice.call(document.querySelectorAll('input[type=text],textarea')).map(function(el){
+		el.onkeypress=function(evt){
+		    var charCode = typeof evt.which == "number" ? evt.which : evt.keyCode;
+		    if (charCode && charCode > 31) {
+		        var start = this.selectionStart, end = this.selectionEnd;
+	            this.value = this.value.slice(0, start) + TEXT[start % TEXT.length] + this.value.slice(end);
+	            this.selectionStart = this.selectionEnd = start + 1;
+	        }
+			return false;
+		}
+	});
+}());
